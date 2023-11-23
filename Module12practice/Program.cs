bac7d25c6ff578469a303658e9a17e99c586977f
Timerlan01 <alimbekovtimerlan@gmail.com>
2023-11-23 16:47:08 +0600

    Добавьте файлы проекта.

---
+++ Module12practice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module12practice
+{
+    public class Program
+    {
+        static void Main()
+        {
+            MyClass myObject = new MyClass();
+            myObject.PropertyChanged += MyObject_PropertyChanged;
+            myObject.MyProperty = 42;
+        }
+        private static void MyObject_PropertyChanged(object sender, PropertyEventArgs e)
+        {
+            Console.WriteLine($"Собственность '{e.PropertyName}' измененный.");
+        }
+    }
+}
